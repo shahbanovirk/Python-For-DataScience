d8b07c9172b597e61555340de34b6f25371b4e79
Shah Bano <engr.shahbano@gmail.com>
2021-08-21 18:27:28 +1000

    Add files via upload

---
+++ Datacamp DS with Python/Numpy/firstNumpyArray.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#*************First numpy array*********
+# Create list baseball
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Import the numpy package as np
+import numpy as np
+
+# Create a numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out type of np_baseball
+print(np_baseball)
