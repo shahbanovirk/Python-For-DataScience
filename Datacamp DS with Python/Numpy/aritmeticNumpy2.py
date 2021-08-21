311fd81c82b1a2a411cdb479cf51244cf48132bd
Shah Bano <engr.shahbano@gmail.com>
2021-08-21 19:35:35 +1000

    Add files via upload

---
+++ Datacamp DS with Python/Numpy/aritmeticNumpy2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+"""
+# height and weight are available as a regular lists
+
+# Import numpy
+import numpy as np
+
+print(np.array([True, 1, 2]) + np.array([3, 4, False]))
+print( np.array([True, 1, 2, 3, 4, False]))
+print(np.array([4, 3, 0]) + np.array([0, 2, 2]))
+print(np.array([1, 1, 2]) + np.array([3, 4, -1]))
